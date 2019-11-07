cb43479f609f453dc50f509d201e4b72d7f491d2
vladibo13 <vladibo13@gmail.com>
2019-11-07 14:40:02 +0200

    test updat for github pages

--- src/components/movie-home/index.tsx
+++ src/components/movie-home/index.tsx
@@ -4,7 +4,7 @@ class MovieHome extends React.Component<any, any> {
 	render() {
 		return (
 			<div>
-				<h1 className="text-center display-2 my-5 display-4">Hello From Movie Home</h1>
+				<h1 className="text-center display-2 my-5 display-4">Hello From Movie Home After Update</h1>
 			</div>
 		);
 	}
