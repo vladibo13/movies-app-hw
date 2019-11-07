41e9ce6159da52683011f7b89350ca945a99fd4b
vladibo13 <vladibo13@gmail.com>
2019-11-07 14:32:07 +0200

    Updates

---
+++ precache-manifest.38496cf5605f95e002e64a8a978dc691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c306de40a30d23d8a19089ae715e88bc",
+    "url": "/movies-app-hw/index.html"
+  },
+  {
+    "revision": "49d2abda3329de9eda66",
+    "url": "/movies-app-hw/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "65471eb5b172cf5aed58",
+    "url": "/movies-app-hw/static/css/main.0b9fcaa6.chunk.css"
+  },
+  {
+    "revision": "49d2abda3329de9eda66",
+    "url": "/movies-app-hw/static/js/2.a52d8704.chunk.js"
+  },
+  {
+    "revision": "65471eb5b172cf5aed58",
+    "url": "/movies-app-hw/static/js/main.f00ce00d.chunk.js"
+  },
+  {
+    "revision": "6d9105601941b8a7c429",
+    "url": "/movies-app-hw/static/js/runtime-main.cb91eaec.js"
+  }
+]);
